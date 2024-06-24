b876ed42020c9bae7178d4a141821d82ea569b7e
muhammadrendi <muhammadrendi360@gmail.com>
2024-06-24 12:16:10 +0700

    Setup Laravel - Implement API Auth Register, Login, Logout menggunakan Sanctum

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'stock',
+        'is_available',
+        'is_favorite',
+        'image',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
